6c61adf5e5d5ccd0ebdfc3575a3589d2bbebf5fd
AKINOLA Victor <akinolavictor26@gmail.com>
2018-11-20 02:21:00 +0100

    Add files via upload

---
+++ js/combineLetters-recursion.js
@@ -0,0 +1,19 @@
+function rps(rounds){
+	let results=[],
+		possibilities=['r', 'p','s'];
+
+	function play(played, rounds){
+		if (rounds === 0){
+			results.push(played);
+			return;
+		}
+		for(var i=0; i<3; i++){
+			play(played+possibilities[i], rounds-1);
+		}
+	}
+
+	play('', rounds);
+	return results;
+}
+
+rps(2);
