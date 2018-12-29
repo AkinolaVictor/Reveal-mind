a01d753901175939fb64303184363a62faec8904
AKINOLA Victor <akinolavictor26@gmail.com>
2018-12-29 23:33:42 +0100

    Add files via upload

---
+++ js/maxsubarr.js
@@ -0,0 +1,45 @@
+let val=[];
+function gen(){
+	let num;
+	for(let i=0; i<10; i++){
+		num=Math.trunc(Math.random()*10);
+		if(i%2==0){
+			val[i]=(1-2)*num
+		}else{
+		val[i]=num;
+		}
+	}
+	return val; 
+	//console.log(val);
+	
+}
+//gen();
+function subarr(){
+	gen();
+	console.log(val);
+	let test=0,take=0,cur,vals=[];
+	for(let i=0; i<val.length; i++){
+		cur=val[i];
+		test=test+cur;
+		if(test>take){
+			take=test;
+			vals.push(cur);
+		}else{
+			//take=0;
+		}
+		
+		if(test<0){
+			test=0;
+		}else{
+			//test=test+cur;
+			//vals.push(cur);
+		}
+	}
+	console.log(take, vals)
+	
+}
+subarr();
+
+
+
+
