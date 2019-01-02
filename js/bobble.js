0853a3fc4debea976dd01040e3419c2ede01e8d1
AKINOLA Victor <akinolavictor26@gmail.com>
2019-01-02 16:07:37 +0100

    Add files via upload

---
+++ js/bobble.js
@@ -0,0 +1,18 @@
+let num=[5,2,1,3,4,9];
+function swap(one,two){
+	var temp=one;
+	one=two;
+	two=temp;
+}
+function bobbleSort(arr){
+	for(let i=0; i<=arr. length; i++){
+		for(let j=arr.length-1; j>=0; j--){
+			if (arr[i]>arr[j]){
+				if(i<j){
+					swap(arr[i],arr[j]);
+				}
+			}
+		}
+	}
+}
+bobbleSort(num);
