1bd5099cc21bbe8ee86fb93269042accf0a606c2
AKINOLA Victor <akinolavictor26@gmail.com>
2019-11-15 09:50:16 +0100

    Update tenary.js

--- js/tenary.js
+++ js/tenary.js
@@ -1,3 +1,3 @@
 // using tenary,  another way to use if and else
-let tryIt=(5>14)?'go now' : "no, don't even go";
+let tryIt=(5>14)?'go now' : "no, don't even go"; and its very fast
 console.log(tryIt);
